b621aefa7d757bdfd4099df7f042f8f0b941a027
Tomasz Grzonkowski <t.grzonkowski@gmail.com>
2021-02-07 16:09:13 +0100

    phototag corrected

--- src/components/PhotoTag/PhotoTag.js
+++ src/components/PhotoTag/PhotoTag.js
@@ -1,16 +1,12 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 
-import { fetchDataAsync } from "../../actions/searchDataActions";
 import styles from "./PhotoTag.module.scss";
 
 const PhotoTag = ({ tag }) => {
-  const dispatch = useDispatch();
   const history = useHistory();
 
   const handleSearch = () => {
-    dispatch(fetchDataAsync(tag));
     history.push(`/results/${tag}`);
   };
   return (
